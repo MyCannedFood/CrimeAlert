01264efe71344d4078e056e71e90988318a534ce
MyCannedFood <sagarawaluya@gmail.com>
2026-07-16 16:27:00 +0700

    connecting frontend to backend

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
         strictPort: true,
         proxy: {
             '/api': {
-                target: 'http://localhost:10000',
+                target: 'http://localhost',
                 changeOrigin: true,
             },
         },
